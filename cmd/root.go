b786a911443bd4551503c4904c33c64219f4bc9e
FuruiKami <sylverstarlight@gmail.com>
2025-09-26 15:17:08 -0600

    root.go compleated

--- cmd/root.go
+++ cmd/root.go
@@ -1,10 +1,13 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
-var rootcmd = &cobra.command{
+var rootcmd = &cobra.Command{
 	Use:   "zero",
 	Short: "zero is a cli tool for performing basic mathematical operations",
 	Long:  "zero is a cli tool for preforming basic mathematical operations - additon, multiplication, division and subtraction",
@@ -15,6 +18,8 @@ var rootcmd = &cobra.command{
 
 func Execute() {
 	if err := rootcmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Oops. An eror while executing zero '%s'\n", err)
+		os.Exit(1)
 
 	}
 }
