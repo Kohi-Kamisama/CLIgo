76826cdbf00e11a5b50dc35ce94a5b3d85c923d0
Kohi-Kamisama <coffeemail48@gmail.com>
2025-09-28 09:04:42 -0700

    Multiplyer body added

--- cmd/zero.go
+++ cmd/zero.go
@@ -33,6 +33,19 @@ func Subtract(first string, second string) (result string) {
 	return fmt.Sprintf("%v", num1-num2)
 }
 
-func Multiply(first string, second string, roundup bool) (result string){
-	
+func Multiply(first string, second string, roundup bool) (result string) {
+	num1, err := strconv.ParseFloat(first, 64)
+	if err != nil {
+		fmt.Println("Error: Firts value is invalid")
+		return
+	}
+	num2, err := strconv.ParseFloat(second, 64)
+	if err != nil {
+		fmt.Println("Error: Second value is invalid")
+		return
+	}
+	if roundup {
+		return fmt.Sprintf("%.2f", num1*num2)
+	}
+	return fmt.Sprintf("%v", num1*num2)
 }
